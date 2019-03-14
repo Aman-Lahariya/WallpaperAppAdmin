4bcc986e70b98dfe745e07ee3dbece405b29fd6c
Aman Lahariya <>
2019-03-14 20:00:51 +0530

    Added Categories & Wallpaper Upload Modules

---
+++ public/js/firebase-Initialization.js
@@ -0,0 +1,22 @@
+
+// Initialize Firebase
+/*var config = {
+    apiKey: "AIzaSyD2ekaBqi5DtuIEWgkV0DEb4dBhsmztpkc",
+    authDomain: "dota-2-wallpaper-app.firebaseapp.com",
+    databaseURL: "https://dota-2-wallpaper-app.firebaseio.com",
+    projectId: "dota-2-wallpaper-app",
+    storageBucket: "dota-2-wallpaper-app.appspot.com",
+    messagingSenderId: "582294044045"
+};
+firebase.initializeApp(config);*/
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyD2ekaBqi5DtuIEWgkV0DEb4dBhsmztpkc",
+    authDomain: "dota-2-wallpaper-app.firebaseapp.com",
+    databaseURL: "https://dota-2-wallpaper-app.firebaseio.com",
+    projectId: "dota-2-wallpaper-app",
+    storageBucket: "dota-2-wallpaper-app.appspot.com",
+    messagingSenderId: "582294044045"
+});
+
